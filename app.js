ec4d3d18198b72652e6bb25defde11a5f4aa415a
Nazibulbd <mdnazibulislamnoyon2023@gmail.com>
2024-03-31 16:23:10 +0600

    site updatd

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let inputs = document.getElementById("inp");
-let text = document.querySelector(".text");
+let edits = document.querySelector(".edit")
+let deletes = document.querySelector(".delete");
 
 function Add (){
     if(inputs.value === ""){
@@ -7,8 +8,9 @@ function Add (){
     } else{
         let newEle =document.createElement("ul");
         newEle.innerHTML = `${inputs.value}<i class="fa-solid fa-trash"></i>`;
-        text.appendChild(newEle);
+        deletes.appendChild(newEle);
         inputs.value="";
+        
         newEle.querySelector("i").addEventListener("click", remove);
         function remove(){
             newEle.remove()
